e81817756cd6398a44764f10d42a696ca26f6866
RobinDejean <robin.dejean@gmail.com>
2026-09-23 12:16:29 +0200

    quelque modifs

--- it-defense/scenes/entities/dangers/danger.gd
+++ it-defense/scenes/entities/dangers/danger.gd
@@ -2,6 +2,7 @@ class_name Danger
 extends Node2D
 @export var speed = 100.0
 @export var max_health = 50
+@export var damage = 20
 
 var health
 var lane
@@ -17,6 +18,15 @@ func _startPos(lane) -> void:
 	position.x = 900
 	position.y = 100 + 200 * lane
 	 
+func _takeDamage(damage) -> void:
+	health -= damage
+	if (health <= 0):
+		die()
+		
+func die() -> void:
+	queue_free()
+
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
